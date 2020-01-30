7a81aaee3d574380ec4afba455d7f83b61ef6587
cishocksr <cishockleysr@gmail.com>
2020-01-29 23:06:36 -0500

    Put complete

--- server.js
+++ server.js
@@ -41,4 +41,18 @@ server.post('/', async (req, res) => {
   }
 });
 
+server.put('/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const acctUpdate = await db('accounts')
+      .where('id', id)
+      .update(req.body);
+    res.status(200).json({ updated: acctUpdate });
+  } catch (err) {
+    res.status(500).json({
+      message: 'Failed to update post'
+    });
+  }
+});
+
 module.exports = server;
